072fd204981898d25aed29cc1f3d4e34f6e2461e
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2015-12-02 23:03:43 +0100

    initial version

---
+++ src/main/java/deors/demos/web/boot/GreetingController.java
@@ -0,0 +1,27 @@
+package deors.demos.web.boot;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "hello, %s!";
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping("/greeting2")
+    public Greeting greeting2(@RequestParam(value = "name", defaultValue = "world") String name) {
+
+        return new Greeting(99, "hardcoded string " + name);
+    }
+}
